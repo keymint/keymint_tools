3fbb492c07fc99b7386aa56d0ac40f30ef6a99b7
ruffsl <roxfoxpox@gmail.com>
2018-10-19 15:11:32 -0700

    Fix arg strings more consistent

--- keymint_tools/command/keystore/verb/create_pkg/__init__.py
+++ keymint_tools/command/keystore/verb/create_pkg/__init__.py
@@ -47,12 +47,12 @@ class CreatePkgVerb(VerbExtension):
         )
         arg.completer = FilesCompleter()
         arg = parser.add_argument(
-            '--public_space',
+            '--public-space',
             help="Path to the public space (default 'CWD/public')",
         )
         arg.completer = FilesCompleter()
         arg = parser.add_argument(
-            '--private_space',
+            '--private-space',
             help="Path to the private space (default 'CWD/private')",
         )
 
